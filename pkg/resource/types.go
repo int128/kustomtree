3afbad7006e2115513a01bd205046f6915b4458d
Hidetake Iwata <int128@gmail.com>
2020-07-20 10:53:44 +0900

    Use lowercase of kind as directory name

--- pkg/resource/types.go
+++ pkg/resource/types.go
@@ -2,6 +2,7 @@ package resource
 
 import (
 	"fmt"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -18,20 +19,11 @@ type Resource struct {
 func (r *Resource) DesiredPath() string {
 	gvk := fmt.Sprintf("%s/%s", r.APIVersion, r.Kind)
 	switch gvk {
-	case "apps/v1/Deployment":
-		return fmt.Sprintf("deployment/%s.yaml", r.Metadata.Name)
-	case "argoproj.io/v1alpha1/Rollout":
-		return fmt.Sprintf("rollout/%s.yaml", r.Metadata.Name)
-	case "batch/v1beta1/CronJob":
-		return fmt.Sprintf("cronjob/%s.yaml", r.Metadata.Name)
-	case "v1/Service":
-		return fmt.Sprintf("service/%s.yaml", r.Metadata.Name)
 	case "autoscaling/v1/HorizontalPodAutoscaler":
 		return fmt.Sprintf("hpa/%s.yaml", r.Metadata.Name)
 	case "policy/v1beta1/PodDisruptionBudget":
 		return fmt.Sprintf("pdb/%s.yaml", r.Metadata.Name)
-	case "v1/ConfigMap":
-		return fmt.Sprintf("configmap/%s.yaml", r.Metadata.Name)
 	}
-	return ""
+	lowerKind := strings.ToLower(r.Kind)
+	return fmt.Sprintf("%s/%s.yaml", lowerKind, r.Metadata.Name)
 }
